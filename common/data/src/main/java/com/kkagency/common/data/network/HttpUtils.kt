bff9f60fd19d1778560d8d67363fd99970e91b5a
Kadir Kertis <kadir@mamboemm.com>
2025-08-22 17:14:56 +0300

    Feat(common-data): Implement Ktor HTTP client utilities and error handling
    Introduced a set of Kotlin extension functions for Ktor's HttpClient
    in the common.data module to streamline GET, POST, and DELETE requests.

---
+++ common/data/src/main/java/com/kkagency/common/data/network/HttpUtils.kt
@@ -0,0 +1,108 @@
+package com.kkagency.common.data.network
+
+import com.kkagency.common.data.BuildConfig
+import com.kkagency.common.domain.util.DataError
+import com.kkagency.common.domain.util.Result
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.request.url
+import io.ktor.client.statement.HttpResponse
+import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.CancellationException
+import kotlinx.serialization.SerializationException
+
+suspend inline fun <reified Response: Any> HttpClient.get(
+    route: String,
+    queryParameters: Map<String, Any?> = mapOf()
+): Result<Response, DataError.Network> {
+    return safeCall {
+        get {
+            url(constructRoute(route))
+            queryParameters.forEach { (key, value) ->
+                parameter(key, value)
+            }
+        }
+    }
+}
+
+suspend inline fun <reified Request,reified Response: Any> HttpClient.post(
+    route: String,
+    body: Request,
+): Result<Response, DataError.Network> {
+    return safeCall {
+        post {
+            url(constructRoute(route))
+            setBody(body)
+        }
+    }
+}
+
+suspend inline fun <reified Response: Any> HttpClient.delete(
+    route: String,
+    queryParameters: Map<String, Any?> = mapOf()
+): Result<Response, DataError.Network> {
+    return safeCall {
+        delete {
+            url(constructRoute(route))
+            queryParameters.forEach { (key, value) ->
+                parameter(key, value)
+            }
+        }
+    }
+}
+
+suspend inline fun <reified T> safeCall(call: () -> HttpResponse): Result<T, DataError.Network> {
+    val response = try {
+        call()
+    } catch (e: UnresolvedAddressException) {
+        e.printStackTrace()
+        return Result.Error(DataError.Network.NO_INTERNET)
+    } catch (e: SerializationException) {
+        e.printStackTrace()
+        return Result.Error(DataError.Network.SERIALIZATION_ERROR)
+
+    } catch (e: Exception) {
+        e.printStackTrace()
+        if (e is CancellationException) throw e
+        return Result.Error(DataError.Network.UNKNOWN)
+
+    }
+
+    return responseToResult(response)
+}
+
+suspend inline fun <reified T> responseToResult(response: HttpResponse): Result<T, DataError.Network> {
+    return when (response.status.value) {
+        in 200..299 -> Result.Success(response.body<T>())
+        400 -> Result.Error(DataError.Network.BAD_REQUEST)
+        401 -> Result.Error(DataError.Network.UNAUTHORIZED)
+        403 -> Result.Error(DataError.Network.FORBIDDEN)
+        404 -> Result.Error(DataError.Network.NOT_FOUND)
+        405 -> Result.Error(DataError.Network.METHOD_NOT_ALLOWED)
+        409 -> Result.Error(DataError.Network.CONFLICT)
+        415 -> Result.Error(DataError.Network.UNSUPPORTED_MEDIA_TYPE)
+        429 -> Result.Error(DataError.Network.TOO_MANY_REQUESTS)
+
+        500 -> Result.Error(DataError.Network.INTERNAL_SERVER_ERROR)
+        502 -> Result.Error(DataError.Network.BAD_GATEWAY)
+        503 -> Result.Error(DataError.Network.SERVICE_UNAVAILABLE)
+        504 -> Result.Error(DataError.Network.GATEWAY_TIMEOUT)
+
+        else -> Result.Error(DataError.Network.UNKNOWN)
+
+    }
+
+}
+
+fun constructRoute(route: String): String {
+    return when {
+        route.contains(BuildConfig.BASE_URL) -> route
+        route.startsWith("/") -> BuildConfig.BASE_URL + route
+        else -> BuildConfig.BASE_URL + "/$route"
+    }
+}
